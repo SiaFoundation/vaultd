d06753f68e002549083deccb0d1f0046fc8119e7
Nate <me@n8m.us>
2025-04-30 11:07:34 -0700

    remove unused syncer tables

--- persist/sqlite/init.sql
+++ persist/sqlite/init.sql
@@ -14,18 +14,6 @@ CREATE TABLE signing_keys (
 CREATE INDEX signing_keys_seed_id_idx ON signing_keys (seed_id);
 CREATE INDEX signing_keys_seed_id_seed_index_idx ON signing_keys (seed_id, seed_index ASC);
 
-CREATE TABLE syncer_peers (
-	peer_address TEXT PRIMARY KEY NOT NULL,
-	first_seen INTEGER NOT NULL
-);
-
-CREATE TABLE syncer_bans (
-	net_cidr TEXT PRIMARY KEY NOT NULL,
-	expiration INTEGER NOT NULL,
-	reason TEXT NOT NULL
-);
-CREATE INDEX syncer_bans_expiration_idx ON syncer_bans (expiration);
-
 CREATE TABLE global_settings (
 	id INTEGER PRIMARY KEY NOT NULL DEFAULT 0 CHECK (id = 0), -- enforce a single row
 	db_version INTEGER NOT NULL, -- used for migrations
